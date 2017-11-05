1e4d103788852521d559f7d19d03f572fe6f6a4f
Jerrin Finney <jfinn8808@gmail.com>
2017-11-05 15:43:54 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,4 +1,5 @@
-function basicTeenager(age) {if basicTeenager(age){>=19 && <=13
+function basicTeenager(age) {var basicTeenager(age) =13
+  if basicTeenager(age){>=19 && <=13
   console.log('you are a teenager!')}
 }
 
